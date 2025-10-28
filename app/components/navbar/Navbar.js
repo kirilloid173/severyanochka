211f37609389f42b86f875b4907f76756c769467
Amogus <empty>
2025-10-28 17:01:58 +0400

    0.2-header_ready

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -1,7 +1,76 @@
+import './style.scss';
+import Image from 'next/image';
 export default function Navbar() {
     return (
         <div className='navbar'>
-            <h1>Магазин</h1>
+            <div className='navbar__inner-content'>
+                <div className='navbar__left-side'>
+                    <h1 className='navbar__title'>Магазин</h1>
+                    <button className='navbar__button-catalog'>
+                        <Image
+                            src={
+                                '/images/svg/navbar_icons/button_menu_icon.svg'
+                            }
+                            alt='icon'
+                            width={24}
+                            height={24}
+                        />{' '}
+                        Каталог
+                    </button>
+                </div>
+                <div className='navbar__right-side'>
+                    <div className='right-side__ui-button'>
+                        <Image
+                            src={
+                                '/images/svg/navbar_icons/heart_navbar_icon.svg'
+                            }
+                            alt='heart'
+                            width={24}
+                            height={24}
+                        />
+                        <p>Избранное</p>
+                    </div>
+                    <div className='right-side__ui-button'>
+                        <Image
+                            src={'/images/svg/navbar_icons/box_navbar_icon.svg'}
+                            alt='heart'
+                            width={24}
+                            height={24}
+                        />
+                        <p>Заказы</p>
+                    </div>
+                    <div className='right-side__ui-button'>
+                        <Image
+                            src={
+                                '/images/svg/navbar_icons/basket_navbar_icon.svg'
+                            }
+                            alt='heart'
+                            width={24}
+                            height={24}
+                        />
+                        <p>Корзина</p>
+                    </div>
+                    <div className='navbar__submenu'>
+                        <Image
+                            src={
+                                '/images/png/navbar_images/alexandr_small_circle_image.png'
+                            }
+                            alt='icon_avatar'
+                            width={40}
+                            height={40}
+                        />
+                        <p>Алексей</p>
+                        <Image
+                            src={
+                                '/images/svg/navbar_icons/stroke_down_icon.svg'
+                            }
+                            alt='stroke_down'
+                            width={13}
+                            height={7}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
