ffb2b8916c5bd5d0328455def5feb701bf0a1ec9
Amogus <empty>
2025-11-01 18:30:29 +0400

    1.7-adaptive_search_page_ready

--- app/components/footer/index.js
+++ app/components/footer/index.js
@@ -1,41 +1,86 @@
 import './index.scss';
+import './mobile.scss';
 import Image from 'next/image';
 export default function Footer() {
     return (
-        <div className='footer'>
-            <div className='footer__left-side'>
-                <p className='footer__title'>Магазин</p>
-                <p>О компании</p>
-                <p>Контакты</p>
-                <p>Вакансии</p>
-                <p>Статьи</p>
-                <p>Политика обработки персональных данных</p>
-            </div>
-            <div className='footer__right-side'>
-                <div className='right-side__icons'>
-                    <Image
-                        src={'/images/svg/footer/vk_logo_icon.svg'}
-                        alt='vk_logo'
-                        width={24}
-                        height={24}
-                    />
-                    <Image
-                        src={'/images/svg/footer/classmates_logo_icon.svg'}
-                        alt='classmates'
-                        width={20}
-                        height={20}
-                    />
+        <>
+            <div className='footer'>
+                <div className='footer__left-side'>
+                    <p className='footer__title'>Магазин</p>
+                    <p>О компании</p>
+                    <p>Контакты</p>
+                    <p>Вакансии</p>
+                    <p>Статьи</p>
+                    <p>Политика обработки персональных данных</p>
                 </div>
-                <div className='right-side__phone'>
-                    <Image
-                        src={'/images/svg/footer/phone_icon.svg'}
-                        alt='phone'
-                        width={24}
-                        height={24}
-                    />
-                    <p>8 800 777 33 33</p>
+                <div className='footer__right-side'>
+                    <div className='right-side__icons'>
+                        <Image
+                            src={'/images/svg/footer/vk_logo_icon.svg'}
+                            alt='vk_logo'
+                            width={24}
+                            height={24}
+                        />
+                        <Image
+                            src={'/images/svg/footer/classmates_logo_icon.svg'}
+                            alt='classmates'
+                            width={20}
+                            height={20}
+                        />
+                    </div>
+                    <div className='right-side__phone'>
+                        <Image
+                            src={'/images/svg/footer/phone_icon.svg'}
+                            alt='phone'
+                            width={24}
+                            height={24}
+                        />
+                        <p>8 800 777 33 33</p>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            {/*  */}
+
+            <div className='footer-mobile'>
+                <p className='footer-mobile__title'>Магазин</p>
+                <div className='footer-mobile__left-side'>
+                    <div className='left-side__row'>
+                        <p>О компании</p>
+                        <p>Контакты</p>
+                        <p>Вакансии</p>
+                    </div>
+                    <div className='left-side__row'>
+                        <p>Статьи</p>
+                        <p>Политика обработки персональных данных</p>
+                    </div>
+                </div>
+                <div className='footer-mobile__right-side'>
+                    <div className='right-side__phone'>
+                        <Image
+                            src={'/images/svg/footer/phone_icon.svg'}
+                            alt='phone'
+                            width={24}
+                            height={24}
+                        />
+                        <p>8 800 777 33 33</p>
+                    </div>
+                    <div className='right-side__social-media'>
+                        <Image
+                            src={'/images/svg/footer/vk_logo_icon.svg'}
+                            alt='vk_logo'
+                            width={24}
+                            height={24}
+                        />
+                        <Image
+                            src={'/images/svg/footer/classmates_logo_icon.svg'}
+                            alt='classmates'
+                            width={20}
+                            height={20}
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
